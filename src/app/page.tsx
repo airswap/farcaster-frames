38a5c663f34e67cfe69fc8e6ff8448b677ca3bf4
Mike Starr <imhs@protonmail.com>
2024-06-28 11:09:39 +0200

    style layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import { fetchMetadata } from "frames.js/next";
+import { DebugLink } from "@/DebugLink";
+import { Metadata } from "next";
 
-export async function generateMetadata() {
+// TODO: fix postUrl
+// const postUrl = `${process.env["HOST"]}/api/otcUrl`;
+
+// MetaData copied and adapted from here: https://github.com/horsefacts/echo-the-dolphin/blob/main/src/app/page.tsx
+
+export async function generateMetadata(): Promise<Metadata> {
+  const imageUrl = `${process.env["HOST"]}/api/images/start?date=${Date.now()}`;
+
   return {
-    title: "My Page",
-    // ...
+    title: "AirSwap OTC Frames",
+    description: "AirSwap OTC makers can paste OTC URLs for their followers to click in Farcaster",
+    openGraph: {
+      title: "AirSwap OTC Frames",
+      images: [imageUrl],
+    },
+
     other: {
-      // ...
       ...(await fetchMetadata(
         // provide a full URL to your /frames endpoint
         new URL(
@@ -15,10 +28,21 @@ export async function generateMetadata() {
             : "http://localhost:3000"
         )
       )),
+      "fc:frame:image": imageUrl,
+      // "fc:frame:post_url": postUrl,
+      "fc:frame:input:text": "Paste your OTC URL here...",
+      "fc:frame:button:1": "Click to upload OTC URL",
+
     },
   };
 }
 
 export default function Page() {
-  return <span>My existing page</span>;
+  return (
+    <>
+      <h1 className="text-lg font-bold">Welcome AirSwap Makers!</h1>
+      <h2>Your followers can now discover your OTC orders directly within farcaster!</h2>
+      <span>AirSwap x Farcaster <span className="underline font-bold"><DebugLink /></span></span>
+    </>
+  );
 }
